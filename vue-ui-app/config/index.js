7e23c698a8b9b2a1402718f9fbf0bcc40e964d34
wenwen <savinazhao@aliyun.com>
2020-03-24 20:49:37 +0800

    commit

--- vue-ui-app/config/index.js
+++ vue-ui-app/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://121.36.174.250:9000', // 接口的域名
+        target: 'http://122.112.211.146:9000', // 接口的域名
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置，为true的话，请求的header将会设置为匹配目标服务器的规则（Access-Control-Allow-Origin）
         pathRewrite: { '^/api': ''}// //本身的接口地址没有 '/api' 这种通用前缀，所以要rewrite，如果本身有则去掉}
