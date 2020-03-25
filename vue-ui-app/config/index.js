60cd5ea86e00151bfe72f3b60097137885c57d7a
snowzhang <snowzhang@aliyun.com>
2020-03-25 10:44:01 +0800

    ajust config

--- vue-ui-app/config/index.js
+++ vue-ui-app/config/index.js
@@ -13,7 +13,6 @@ module.exports = {
     proxyTable: {
       '/api': {
         target: 'http://122.112.211.146:9000', // 接口的域名
-        // target: 'http://121.36.174.250:9000', // 接口的域名
         //  target: 'http://127.0.0.1:9000', // 接口的域名
         // secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置，为true的话，请求的header将会设置为匹配目标服务器的规则（Access-Control-Allow-Origin）
